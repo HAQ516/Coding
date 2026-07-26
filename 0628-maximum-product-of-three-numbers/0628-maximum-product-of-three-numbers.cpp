798c26ec5780e0677d15b340962d92994a37815f
Hafiz Abdul Qadir <abdulqadirhafiz9@gmail.com>
2026-07-27 02:10:07 +0500

    Time: 3 ms (65.14%), Space: 31.7 MB (10.44%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -4,7 +4,9 @@ public:
         sort(nums.begin(),nums.end());
         int s=nums.size();
         int ans=1;
+        //Pattern: product of first 3 largest
         int a1=nums[s-1]*nums[s-2]*nums[s-3];
+        //Product of first 2 smallers and 1 largest
         int a2=nums[0]*nums[1]*nums[s-1];
         ans=a1>a2?a1:a2;
         return ans;
